b06a37f580582a023f977e70522040ba719c825d
huangdongcheng <934313141@qq.com>
2019-12-07 21:21:06 +0800

    singleton magic static

--- singleton.cpp
+++ singleton.cpp
@@ -18,6 +18,8 @@ public:
 	~Singleton()
 	{
 		cout << "destructor called!" << endl;
+
+		delete instance;
 	}	
 
 	static Singleton* getInstance()
@@ -36,5 +38,38 @@ int main(int argc, char const *argv[])
 {
 	Singleton* instance1 = Singleton::getInstance();
 	Singleton* instance2 = Singleton::getInstance();
+	return 0;
+}
+
+class Singleton
+{
+public:
+	~Singleton()
+	{
+		cout << "destructor called" << endl;
+	}
+
+	Singleton(const Singleton&) = delete;
+	Singleton& operator = (const Singleton&) = delete;
+
+	static Singleton& getInstance()
+	{
+		static Singleton instance;
+
+		return instance;
+	}
+
+private:
+	Singleton() 
+	{
+		cout << "constructor called" << endl;
+	}	
+}
+
+int main(int argc, char const *argv[])
+{
+	Singleton& instance1 = Singleton::getInstance();
+	Singleton& instance2 = Singleton::getInstance();
+	
 	return 0;
 }
